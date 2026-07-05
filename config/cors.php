8a8aafb3907d069120714f29c6b8229c92f74613
ato <kuotsuato59@gmail.com>
2026-07-05 15:46:05 +0530

    Update CORS for production frontend

--- config/cors.php
+++ config/cors.php
@@ -19,8 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
-
+    'allowed_origins' => ['https://kuotsu.vercel.app'],
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
